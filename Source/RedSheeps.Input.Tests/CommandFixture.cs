4bf2fb8e806b0862ba45fc4c872d748dbdf6e3de
Atsushi Nakamura <nuits.jp@live.jp>
2019-02-20 23:02:53 +0900

    Add RedSheeps.Input.Tests project.

---
+++ Source/RedSheeps.Input.Tests/CommandFixture.cs
@@ -0,0 +1,93 @@
+﻿using Xunit;
+
+namespace RedSheeps.Input.Tests
+{
+    public class CommandFixture
+    {
+        [Fact]
+        public void ExecuteWhenNoArgumentsAndUnspecified()
+        {
+            var isCalled = false;
+            var command = new Command(() => isCalled = true);
+            command.Execute(null);
+            Assert.True(isCalled);
+        }
+
+        [Fact]
+        public void ExecuteWhenNoArgumentsAndNoArguments()
+        {
+            var isCalled = false;
+            var command = new Command(() => isCalled = true, () => true);
+            command.Execute(null);
+            Assert.True(isCalled);
+        }
+
+        [Fact]
+        public void ExecuteWhenObjectArgumentAndUnspecified()
+        {
+            object argument = null;
+            var command = new Command(x => argument = x);
+            var param = new object();
+            command.Execute(param);
+            Assert.Equal(param, argument);
+        }
+
+        [Fact]
+        public void ExecuteWhenObjectArgumentAndObjectArgument()
+        {
+            object argument = null;
+            var command = new Command(x => argument = x, x => true);
+            var param = new object();
+            command.Execute(param);
+            Assert.Equal(param, argument);
+        }
+
+        [Fact]
+        public void CanExecuteWhenNoArgumentsAndUnspecified()
+        {
+            var command = new Command(() => { });
+            Assert.True(command.CanExecute(null));
+        }
+
+        [Fact]
+        public void CanExecuteWhenNoArgumentsAndNoArguments()
+        {
+            var command = new Command(() => { }, () => false);
+            Assert.False(command.CanExecute(null));
+        }
+
+        [Fact]
+        public void CanExecuteWhenObjectArgumentAndUnspecified()
+        {
+            var command = new Command(_ => { });
+            Assert.True(command.CanExecute(null));
+        }
+
+        [Fact]
+        public void CanExecuteWhenObjectArgumentAndObjectArgument()
+        {
+            object argument = null;
+            var command = new Command(x => argument = x, x =>
+            {
+                argument = x;
+                return false;
+            });
+            var param = new object();
+            Assert.False(command.CanExecute(param));
+            Assert.Equal(param, argument);
+        }
+
+        [Fact]
+        public void NotifyCanExecuteChanged()
+        {
+            var command = new Command(() => { });
+            // Confirm that no exception is thrown
+            command.NotifyCanExecuteChanged();
+
+            var isCalledCanExecuteChanged = false;
+            command.CanExecuteChanged += (sender, args) => isCalledCanExecuteChanged = true;
+            command.NotifyCanExecuteChanged();
+            Assert.True(isCalledCanExecuteChanged);
+        }
+    }
+}
---
+++ Source/RedSheeps.Input.Tests/CommandTFixture.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+
+namespace RedSheeps.Input.Tests
+{
+    public class CommandTFixture
+    {
+        [Fact]
+        public void ExecuteWhenGenericArgumentAndUnspecified()
+        {
+            object argument = null;
+            var command = new Command<bool>(x => argument = x);
+            command.Execute(true);
+            Assert.Equal(true, argument);
+        }
+
+        [Fact]
+        public void ExecuteWhenGenericArgumentAndGenericArgument()
+        {
+            object argument = null;
+            var command = new Command<bool>(x => argument = x, x => true);
+            command.Execute(true);
+            Assert.Equal(true, argument);
+            Assert.Throws<InvalidCastException>(() => command.Execute(string.Empty));
+        }
+
+        [Fact]
+        public void CanExecuteWhenGenericArgumentAndUnspecified()
+        {
+            var command = new Command<bool>(_ => { });
+            Assert.True(command.CanExecute(null));
+        }
+
+        [Fact]
+        public void CanExecuteWhenGenericArgumentAndGenericArgument()
+        {
+            object argument = null;
+            var command = new Command<bool>(x => argument = x, x =>
+            {
+                argument = x;
+                return false;
+            });
+            Assert.False(command.CanExecute(true));
+            Assert.Equal(true, argument);
+            Assert.Throws<InvalidCastException>(() => command.Execute(string.Empty));
+        }
+    }
+}
